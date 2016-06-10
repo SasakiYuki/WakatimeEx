8dba3675dba4d0456d1cadcd548e371359b1f1e0
So-ma <kaso199553@yahoo.co.jp>
2016-06-10 18:37:46 +0900

    add SearchEntity

---
+++ WakatimeEX/app/src/main/java/com/wacode/yuki/wakatimeex/Entity/SearchListEntity.java
@@ -0,0 +1,25 @@
+package com.wacode.yuki.wakatimeex.Entity;
+
+/**
+ * Created by Riberd on 2016/06/08.
+ */
+public class SearchListEntity {
+    private String name;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
